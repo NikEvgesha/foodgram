d33334ef340dc97fd1360b4882b4342430bda2db
tentacos <tentacos@student.21-school.ru>
2024-12-18 16:57:37 +0300

    github actions

--- backend/dbclean.sh
+++ backend/dbclean.sh
@@ -1,6 +1,4 @@
-rm users/migrations/0*.py recipes/migrations/0*.py
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py import_csv
 python manage.py collectstatic
 cp -r /app/collected_static/. /backend_static/static/
